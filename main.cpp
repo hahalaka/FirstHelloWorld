ab4903437d4647521e1da92982184e65a6109b89
cps11 <cpseleven@gmail.com>
2014-05-19 15:43:40 +0800

    Update main.cpp
    yah~yah~ i know~~~

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+using namespace std;
+
 int main() 
 {
-  cout << "hello world!\n";
+  cout << "hello world!\n"; 
+  cout << "this is added by pengsen cheng!" << endl;
   return 0;
 }
